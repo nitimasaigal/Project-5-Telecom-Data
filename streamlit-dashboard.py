9524b7e68e62eebfb2609e66233bc4913ed44233
Nitima saigal <146649752+nitimasaigal@users.noreply.github.com>
2024-02-04 14:09:41 +0530

    Add files via upload

--- streamlit-dashboard.py
+++ streamlit-dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 from PIL import Image
